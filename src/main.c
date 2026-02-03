67919b070ffd504f5032940dd460912143d073db
Nabir14 <82253045+Nabir14@users.noreply.github.com>
2026-02-03 15:29:21 +0600

    VELOCITY

--- src/main.c
+++ src/main.c
@@ -3,26 +3,39 @@
 #include <raylib.h>
 #include <stdbool.h>
 
-#define GRAVITY -3.0f
+#define GRAVITY -98.0f
 #define MAX_HEIGHT  -72.0f
 #define MIN_HEIGHT 350.0f
 #define FONT_SIZE 32
+#define PLAYER_JUMP_VELOCITY 64.0f
+#define PLAYER_SPIN_SPEED 1.0f
 
 typedef struct {
     Vector2 position;
     Vector2 scale;
     float rotation;
     Texture texture;
+    Vector2 origin;
 } Sprite;
+ 
+typedef struct {
+    Sprite sprite;
+    Vector2 velocity;
+} Player;
 
-void apply_gravity(Sprite* sprite) {
-    sprite->position.y -= GRAVITY;
+void ApplyGravity(Player* player, float delta) {
+    player->velocity.y -= GRAVITY * delta;
+}
+
+void ApplyVelocity(Player* player, float delta) {
+        player->sprite.position.x += player->velocity.x * delta;
+        player->sprite.position.y += player->velocity.y * delta;
 }
 
 void DrawSprite(Sprite* sprite) {
     Rectangle sourceRect = {0.0f, 0.0f, (float)sprite->texture.width, (float)sprite->texture.height};
     Rectangle destRect = {sprite->position.x, sprite->position.y, (float)sprite->texture.width * sprite->scale.x, (float)sprite->texture.height * sprite->scale.y};
-    DrawTexturePro(sprite->texture, sourceRect, destRect, (Vector2){0.0f, 0.0f}, sprite->rotation, WHITE);
+    DrawTexturePro(sprite->texture, sourceRect, destRect, sprite->origin, sprite->rotation, WHITE);
 }
 
 int main()
@@ -42,42 +55,50 @@ int main()
         .position = (Vector2){0.0f, 0.0f},
         .scale = (Vector2){ 1.5f, 1.0f},
         .rotation = 0.0f,
-        .texture = LoadTexture("../resources/bg.png")
+        .texture = LoadTexture("../resources/bg.png"),
+        .origin = (Vector2){0.0f, 0.0f}
     };
     
-    Sprite player = {
-        .position = (Vector2){240.0f, 0.0f},
-        .scale = (Vector2){ 0.25f, 0.25f },
-        .rotation = 0.0f,
-        .texture = LoadTexture("../resources/face.png")
+    Player player = {
+        .sprite = { 
+            .position = (Vector2){240.0f, 0.0f},
+            .scale = (Vector2){ 0.25f, 0.25f },
+            .rotation = 0.0f,
+            .texture = LoadTexture("../resources/face.png"),
+            .origin = (Vector2){0.0f, 0.0f}
+        },
+        .velocity = (Vector2){ 0.0f, 0.0f }
     };
     
     Sprite monster = {
         .position = (Vector2){0.0f, 0.0f},
         .scale = (Vector2){ 1.3f, 1.0f},
         .rotation = 0.0f,
-        .texture = LoadTexture("../resources/monster.png")
+        .texture = LoadTexture("../resources/monster.png"),
+        .origin = (Vector2){0.0f, 0.0f}
     };
     
     // Game
-    memset(&text_buffer, 0, sizeof(text_buffer));
-    
     while (!WindowShouldClose())
-    {        
+    {
+        float delta = GetFrameTime();
+        
         if (!game_over) {
-            player.rotation += 1.0f;
+            player.sprite.rotation += 1.0f;
             
             if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-                player.position.y -= 50.0f;
+                player.velocity.y -= 64.0f;
                 score++;
                 sprintf(text_buffer, "Score: %d", score);
+            } else {
+                ApplyGravity(&player, delta);
             }
         
-            if (player.position.y < MAX_HEIGHT || player.position.y > MIN_HEIGHT) {
+            if (player.sprite.position.y < MAX_HEIGHT || player.sprite.position.y > MIN_HEIGHT) {
                 game_over = true;
             }
             
-            apply_gravity(&player);
+            ApplyVelocity(&player, delta);
         }
         
         BeginDrawing();
@@ -86,7 +107,7 @@ int main()
             DrawSprite(&bg);
             
             if (!game_over) {
-                DrawSprite(&player);
+                DrawSprite(&player.sprite);
             }
             
             DrawSprite(&monster);
@@ -104,7 +125,7 @@ int main()
     }
     
     UnloadTexture(bg.texture);
-    UnloadTexture(player.texture);
+    UnloadTexture(player.sprite.texture);
     UnloadTexture(monster.texture);
 
     CloseWindow();
